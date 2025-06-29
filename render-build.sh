66bbfa1aed33e82ff06fb8944f53ce5057c89e9d
Punfetto <37002332+Punfetto@users.noreply.github.com>
2025-06-29 17:35:41 +0700

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y wget gnupg ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libnspr4 libnss3 libxcomposite1 libxdamage1 libxrandr2 xdg-utils --no-install-recommends
